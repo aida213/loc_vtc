0ec8ce17ad327517bbce7921954ff09809907505
aida213 <68588075+aida213@users.noreply.github.com>
2021-02-01 16:50:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+imageUrls: [
+    
+        'background.jpg',
+    
+        'graphisme.png',
+    
+        'vehicule1.png',
+    
+        ]
+
+        imageUrls: [];
+        duration: 5000;
+        fadeSpeed: 1000; 
+        backgroundSize: auto; 
